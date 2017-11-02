526fa82d41c0e9f31edc750a68c4f8a718f00668
AsifAhmedShodip <asifahmedshodip@gmail.com>
2017-11-03 01:26:44 +0600

    Change to test

--- app/src/main/java/com/example/aniomi/myapplication/NoticeBoard.java
+++ app/src/main/java/com/example/aniomi/myapplication/NoticeBoard.java
@@ -15,7 +15,7 @@ public class NoticeBoard extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        
+
         return inflater.inflate(R.layout.fragment_notice_board, container, false);
     }
 
