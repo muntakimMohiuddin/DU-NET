1454f7d158ff2de59b277406b127f4431df30cb7
AsifAhmedShodip <asifahmedshodip@gmail.com>
2017-11-02 22:28:05 +0600

    Change to test

--- app/src/main/java/com/example/aniomi/myapplication/NoticeBoard.java
+++ app/src/main/java/com/example/aniomi/myapplication/NoticeBoard.java
@@ -9,14 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link NoticeBoard.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link NoticeBoard#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class NoticeBoard extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
