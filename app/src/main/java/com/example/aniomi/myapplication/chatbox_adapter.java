2941d44a2165274d5e2b7808dccc80ad24f84f62
AsifAhmedShodip <asifahmedshodip@gmail.com>
2017-11-04 15:56:16 +0600

    Merge branch 'master' of https://github.com/AsifAhmedShodip/MyApplication
    # Conflicts:
    #	app/src/main/res/layout/fragment_event_tabs.xml

--- app/src/main/java/com/example/aniomi/myapplication/chatbox_adapter.java
+++ app/src/main/java/com/example/aniomi/myapplication/chatbox_adapter.java
@@ -45,15 +45,15 @@ public class chatbox_adapter extends RecyclerView.Adapter<chatbox_adapter.sViewH
     public class sViewHolder extends RecyclerView.ViewHolder
     {
 
-        public TextView text1;
+        public TextView text1,text2;
         public CardView cv;
-        public ImageView imageView1;
+        public ImageView imageView1,imageView2;
         public sViewHolder(View itemView) {
             super(itemView);
             text1=itemView.findViewById(R.id.t1);
-            //text2=itemView.findViewById(R.id.t2);
+            text2=itemView.findViewById(R.id.t2);
             imageView1=itemView.findViewById(R.id.imageView1);
-            //imageView2=itemView.findViewById(R.id.imageView2);
+            imageView2=itemView.findViewById(R.id.imageView2);
             cv=itemView.findViewById(R.id.cv);
 
         }
@@ -70,16 +70,34 @@ public class chatbox_adapter extends RecyclerView.Adapter<chatbox_adapter.sViewH
     public void onBindViewHolder(sViewHolder holder, int position) {
 
         Messanger temp=list.get(position);
+        if(temp.sender.equals(Students.current.uid))
+        {
+            holder.text1.setVisibility(View.GONE);
+            holder.imageView1.setVisibility(View.GONE);
+            holder.text2.setVisibility(View.VISIBLE);
+            holder.imageView2.setVisibility(View.VISIBLE);
+        }
+        else
+        {
+            holder.text2.setVisibility(View.GONE);
+            holder.imageView2.setVisibility(View.GONE);
+            holder.text1.setVisibility(View.VISIBLE);
+            holder.imageView1.setVisibility(View.VISIBLE);
+        }
+        holder.text1.setText(temp.getMail());
+        holder.text2.setText(temp.getMail());
+        MainActivity.setImageFromStorage(context,"images/"+temp.sender+".jpg",holder.imageView1);
+        MainActivity.setImageFromStorage(context,"images/"+temp.sender+".jpg",holder.imageView2);
 
             /*View B=holder.text2;
             B.setVisibility(View.INVISIBLE);
             B=holder.imageView2;
             B.setVisibility(View.INVISIBLE);*/
             //holder.text2.setText("");
-            holder.text1.setText(temp.getMail());
+            //holder.text1.setText(temp.getMail());
             /*TextView someView = holder.text1;
             View root = someView.getRootView();*/
-            if(temp.sender.equals(Students.current.uid)){
+            /*if(temp.sender.equals(Students.current.uid)){
             TextView layout =holder.text1;
             layout.setBackgroundResource(R.drawable.receivers);layout.setTextColor(Color.parseColor("#000000"));}
             else
@@ -96,7 +114,8 @@ public class chatbox_adapter extends RecyclerView.Adapter<chatbox_adapter.sViewH
                     .load(forestRef)
                     .diskCacheStrategy(DiskCacheStrategy.NONE)
                     .transform(new CircleTransform(context))
-                    .into(holder.imageView1);
+                    .into(holder.imageView1);*/
+
 
     }
 
