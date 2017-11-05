458d41d211d89985dc877ec2bd4a53adcc7c5c15
AsifAhmedShodip <shadman.islam.rafi@gmail.com>
2017-11-05 22:51:45 +0600

    design

---
+++ app/src/main/java/com/example/aniomi/myapplication/activity_bus_track.java
@@ -0,0 +1,13 @@
+package com.example.aniomi.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_bus_track extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bus_track);
+    }
+}
